ffd2a706490fffc3719b2221ec463e2d2053e60c
emtsubasa <yagimeemeesann@gmail.com>
2019-09-10 17:18:19 +0900

    complete assignment 2

--- src/index.js
+++ src/index.js
@@ -98,6 +98,17 @@ const row_size = 3;
         const desc = move?
           newbutton :
           'Go to game start';
+        if(move == this.state.stepNumber){
+          return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>
+              <b>
+                {desc}
+              </b>
+            </button>
+          </li>
+          )
+        }
         return (
           <li key={move}>
             <button onClick={() => this.jumpTo(move)}>
